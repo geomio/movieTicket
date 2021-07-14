f9d58e0a309bb856fcc0cd133819a0ea965db392
Geoffry Millner <geoff.millner22@gmail.com>
2021-07-14 13:29:06 -0700

    made prototypes for ticketbook object

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 
 //constructor
+
+function TicketBook() {
+    this.tickets = {};
+    this.currentId = 0;
+  }
+
 function Ticket(movieId, userAge, evening, movieName, ticketTime) {
     this.movieId = movieId;
     this.userAge = userAge;
@@ -30,22 +36,80 @@ Ticket.prototype.calculatePrice = function () {
     
 };
 
+TicketBook.prototype.addTicket = function(ticket) {
+    ticket.id = this.assignId();
+    this.tickets[ticket.id] = ticket;
+  }
+  
+  TicketBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+  }
+  
+  TicketBook.prototype.findTicket = function(id) {
+    if (this.tickets[id] != undefined) {
+      return this.tickets[id];
+    }
+    return false;
+  }
+
+  TicketBook.prototype.deleteTicket = function(id) {
+    if (this.tickets[id] === undefined) {
+      return false;
+    }
+    delete this.tickets[id];
+    return true;
+  }
+
+function ticketLister(toDisplay) {
+    let ticketList = $("#tickets");
+    let htmlForTicketInfo = "";
+    Object.keys(toDisplay.ticket).forEach(function(key) {
+        const ticket = toDisplay.findTicket(key);
+        htmlForTicketInfo += "<li id=" + ticket.id + ">" + ticket.movieName + " " + ticket.ticketTime + "</li>"; 
+    });
+    ticketList.html(htmlForTicketInfo)
+};
+
+let ticketBook = new TicketBook();
+
+
+function showTicket() {
+    const mainTicket = ticketBook.findTicket(mainTicketId);
+    $("#show-ticket").show();
+    $(".movieNameInputArea").html(mainTicket.movieName);
+    $(".showTime").html(mainTicket.ticketTime);
+    $(".price").html(mainTicket.ticketPrice);
+    let buttons = $("#buttons");
+    buttons.empty();
+    buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>")
+};
+
+function attachTicketListeners() {
+    $("ul#tickets").on("click", "li", function() {
+      showTicket(this.id);
+    });
+    $("#buttons").on("click", ".deleteButton", function() {
+      ticketBook.deleteTicket(this.id);
+      $("#show-ticket").hide();
+      displayContactDetails(ticketBook);
+    });
+  };
+  
 
 $(document).ready(function() {
+    attachTicketListeners();
     $('form#userInfoForm').submit(function(event) {
+        event.preventDefault();
         const inputMovie = $('select#movieName').val();
         const testName = $('#movieName').find(":selected").data('title')
         const testTime = $('#timeOfDay').find(":selected").data('time')
         const inputAge = parseInt($('input#userAge').val());
         const inputTime = $('select#timeOfDay').val();
         let userTicket = new Ticket( inputMovie, inputAge, inputTime, testName, testTime)
+        ticketBook.addTicket(userTicket);
         userTicket.calculatePrice();
-        console.log("test time:", inputTime)
-        console.log("age test:",inputAge)
-        console.log("test movie:", inputMovie)
-        console.log("multi data test:", testName)
-        console.log("multi data test2:", testTime)
-        console.dir(userTicket);
-        event.preventDefault();
+        console.log("this to stop");
+        $("#userTicketList").show();  
     });
   });
